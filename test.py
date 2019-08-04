16b8b088c6acd7d380cae14ae266fb070bd9d312
ihartb <bharu2999@gmail.com>
2019-08-04 15:47:52 -0400

    Completed Perceptron and features extractor.
    check main for naive bayes digit usage with processData, very last lines are for help or text me ;) lol

--- test.py
+++ test.py
@@ -1,61 +1,83 @@
 import numpy as np
 
-fileName = 'Data/digitdata/trainingimages.txt'
+fileName = 'Data/facedata/facedatatrainlabels.txt'
 f = open(fileName)
 lines = f.readlines()
 f.close()
 
-del lines[28*3]
-images = [np.zeros((28, 28)) for i in range( 3 ) ]
-
-j=0
-for image in images:
-    x = np.nditer(image, op_flags=['writeonly'])
-    i = 0
-    for line in lines:
-        for c in line:
-            if c == '\n':
-                continue
-            elif c == '#':
-                x[0] = 1
-            elif c == '+':
-                x[0] = .5
-            x.iternext()
-            if(x.finished):break
-        if(x.finished):break
-print(images)
-f.close()
-
-imagez = [np.random.randint(0, 10, (8, 8)) for i in range(0,3)]
-features = [np.zeros(( img.size//4, 1) )for img in imagez]
-for image, ft in zip(imagez, features):
-    x = np.nditer(ft, op_flags=['writeonly'])
-    for i in range(0, 8, 2):
-        for j in range(0, 8, 2):
-            print(image)
-            submatrix = image[i:i + 2, j:j + 2]
-            print(submatrix)
-            print(np.mean(submatrix))
-            x[0] = np.mean(submatrix)
-            print(x[0])
-            x.iternext()
-            if (x.finished): break
-        if (x.finished): break
-
-print(features)
-
-fileName = 'Data/digitdata/traininglabels.txt'
-f = open(fileName)
-lines = f.readlines()
-f.close()
-
+# convert list of each line into integer list
 integers = list(map(int, lines))
 
-labels = [np.zeros((10, 1)) for i in range(len(lines))]
+# initialize 10x1 numpy vector to represent labels
+labels = [np.zeros((1, 1)) for i in range(len(lines))]
 
-for i,vector in zip(integers, labels):
-    print(i)
-    vector[i][0] = 1
-    print(vector)
+# make element at index of label to 1
+for i, vector in zip(integers, labels):
+    vector[0][0] = i
 
 print(labels)
+
+
+
+
+# fileName = 'Data/digitdata/trainingimages.txt'
+# f = open(fileName)
+# lines = f.readlines()
+# f.close()
+#
+# del lines[28*20]
+# images = []
+#
+# i=0
+# k=0
+# image = np.zeros((28,28))
+# while k < len(lines):
+#     if (i==28):
+#         i=0
+#         images.append(image)
+#         image = np.zeros((28,28))
+#     line = lines[k]
+#     for c,j in zip(line,range(0,28)):
+#         if c == '\n':
+#             continue
+#         elif c == '#':
+#             image[i][j] = 1
+#         elif c == '+':
+#             image[i][j] = .5
+#     k+=1
+#     i+=1
+#
+#
+# print(images)
+
+# features = [np.zeros(( img.size//4, 1) )for img in images]
+# for image, ft in zip(images, features):
+#     x = np.nditer(ft, op_flags=['writeonly'])
+#     for i in range(0, 28, 2):
+#         for j in range(0, 28, 2):
+#             submatrix = image[i:i + 2, j:j + 2]
+#             x[0] = np.mean(submatrix)
+#             x.iternext()
+#             if (x.finished): break
+#         if (x.finished): break
+#
+#
+# fileName = 'Data/digitdata/traininglabels.txt'
+# f = open(fileName)
+# lines = f.readlines()
+# f.close()
+#
+# integers = list(map(int, lines))
+# print(len(lines))
+#
+# labels = [np.zeros((10, 1)) for i in range(len(lines))]
+#
+# for i,vector in zip(integers, labels):
+#     vector[i][0] = 1
+#
+#
+# print(features)
+# print(images)
+#
+# # print(labels)
+
